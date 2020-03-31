60df21771202245b2529253c4f8877dd585c9aff
Santiago Badia <santiago.badia@monash.edu>
2020-03-31 13:48:30 +1100

    Some comments in design

--- design.jl
+++ design.jl
@@ -12,56 +12,75 @@
 # The first we need is an abstract type representing the operator A:
 #
 
+
+# @santiagobadia Here you are defining A(t,u,u_t) in A(t,u,u_t) = 0.
 abstract type ODEOperator <: GridapType end
+# @santiagobadia: There is quite a lot of noise about what an ODE is, i.e.,
+# whether it requires a non-singular Jacobian wrt u_t (DAE in this case) or not.
+# I decided to use the term TimeStepper to circumvent this issue. In some places
+# they use the term implicit ODEs to what I propose to use.
+
+# For me the ODEOperator should be quite simple. In the Gridap FEM context,
+# it will take a residual r(t,u,u_t,v), jacobian_unk(t,u,u_t,v) and
+# jacobian_unk_t(t,u,u_t,v). In any case, there are some things to be
+# considered when designing this object. TrialSpaces can also depend on time,
+# due to time dependent boundary conditions. Also, new transient functions
+# will be needed, e.g., u_D(x,t). We must think how to implement all this.
 
 # This ones represents the value `A(t,u,v)`
-function residual!(r::AbstractVector,op::ODEOperator,t::Real,u::AbstractVector,v::AbstractVector)
+function residual!(r::AbstractVector,op::ODEOperator,t::Real,u::AbstractVector,u_t::AbstractVector)
   @abstractmethod
 end
+# @santiagobadia : Agreed
 
-function allocate_residual(op::ODEOperator,t::Real,u::AbstractVector,v::AbstractVector)
+
+function allocate_residual(op::ODEOperator,t::Real,u::AbstractVector,u_t::AbstractVector)
   @abstractmethod
 end
+# @santiagobadia : Agreed
 
 # This one represents `[∂A/∂u](t,u,v)`
-# Better function name?
-function jacobian_u!(j_u::AbstractMatrix,op::ODEOperator,t::Real,u::AbstractVector,v::AbstractVector)
+function jacobian_unk!(j_u::AbstractMatrix,op::ODEOperator,t::Real,u::AbstractVector,u_t::AbstractVector)
   @abstractmethod
 end
+# @santiagobadia : Agreed
+# @santiagobadia : I think unk and unk_t or u u_t is more revealing than u v
+# Not exposed to user anyway
 
 # This one represents `[∂A/∂v](t,u,v)`
-# Better function name?
-function jacobian_v!(j_v::AbstractMatrix,op::ODEOperator,t::Real,u::AbstractVector,v::AbstractVector)
+function jacobian_unk_t!(j_u_t::AbstractMatrix,op::ODEOperator,t::Real,u::AbstractVector,u_t::AbstractVector)
   @abstractmethod
 end
+# @santiagobadia : Agreed
 
-function allocate_jacobian(op::ODEOperator,t::Real,u::AbstractVector,v::AbstractVector)
+function allocate_jacobian(op::ODEOperator,t::Real,u::AbstractVector,u_t::AbstractVector)
   @abstractmethod
 end
+# @santiagobadia : Agreed
 
 # EXAMPLE:
 
 # Toy linear ODE with 2 DOFs
-# v_1 = a * u_1
-# v_2 = b * u_1 + c * u_2 
+# u_1_t = a * u_1
+# u_2_t = b * u_1 + c * u_2
 struct ODEOperatorMock{T<:Real} <: ODEOperator
   a::T
   b::T
   c::T
 end
 
-function residual!(r::AbstractVector,op::ODEOperatorMock,t::Real,u::AbstractVector,v::AbstractVector)
+function residual!(r::AbstractVector,op::ODEOperatorMock,t::Real,u::AbstractVector,u_t::AbstractVector)
   r .= 0
-  r[1] = - v[1] + op.a * u[1]
-  r[2] = - v[2] + op.b * u[1] + op.c * u[2]
+  r[1] = - u_t[1] + op.a * u[1]
+  r[2] = - u_t[2] + op.b * u[1] + op.c * u[2]
   r
 end
 
-function allocate_residual(op::ODEOperatorMock,t::Real,u::AbstractVector,v::AbstractVector)
+function allocate_residual(op::ODEOperatorMock,t::Real,u::AbstractVector,u_t::AbstractVector)
   zeros(2)
 end
 
-function jacobian_u!(j_u::AbstractMatrix,op::ODEOperatorMock,t::Real,u::AbstractVector,v::AbstractVector)
+function jacobian_unk!(j_u::AbstractMatrix,op::ODEOperatorMock,t::Real,u::AbstractVector,u_t::AbstractVector)
   j_u .= 0
   j_u[1,1] = op.a
   j_u[2,1] = op.b
@@ -69,14 +88,14 @@ function jacobian_u!(j_u::AbstractMatrix,op::ODEOperatorMock,t::Real,u::Abstract
   j_u
 end
 
-function jacobian_v!(j_v::AbstractMatrix,op::ODEOperatorMock,t::Real,u::AbstractVector,v::AbstractVector)
-  j_v .= 0
-  j_v[1,1] = -1
-  j_v[2,2] = -1
-  j_v
+function jacobian_unk_t!(j_u_t::AbstractMatrix,op::ODEOperatorMock,t::Real,u::AbstractVector,u_t::AbstractVector)
+  j_u_t .= 0
+  j_u_t[1,1] = -1
+  j_u_t[2,2] = -1
+  j_u_t
 end
 
-function allocate_jacobian(op::ODEOperatorMock,t::Real,u::AbstractVector,v::AbstractVector)
+function allocate_jacobian(op::ODEOperatorMock,t::Real,u::AbstractVector,u_t::AbstractVector)
   zeros(2,2)
 end
 
@@ -89,10 +108,10 @@ function solve_step!(
   uF::AbstractVector,solver::ODESolver,op::ODEOperator,u0::AbstractVector,t0::Real) # -> (uF,tF)
   @abstractmethod
 end
-
+# @santiagobadia : Agreed
 
 # Assuming that solve_step! is defined, we can define the following solve function for all ODESolver objects:
-# Given an ODE represented by an ODEOperator `op` an initial condition `u0` and a time span `t0,tF`, solve 
+# Given a ODE represented by an ODEOperator `op` an initial condition `u0` and a time span `t0,tF`, solve
 # this ODE lazily with the provided solver. I.e., return a ODESolution object (see below)
 # that can be iterated to recover the pairs (u_n, t_n) at
 # each computed step.
@@ -104,6 +123,9 @@ end
 # dt = 0.01
 # nls = NLSolver()
 # solver = BackwardEuler(nls,dt)
+# @santiagobadia: Think about dt that can change in time, e.g., using a mutable
+# array that is just dt all time steps but that could allow more complicated situations,
+# e.g., time adaptivity
 #
 # u0 = [0,0]
 # steps = solve(solve,op,u0,0,10)
@@ -119,8 +141,11 @@ function solve(
   GenericODESolution(solver,op,u0,t0,tF)
 end
 
-## This is what you have called TimeStepper
 abstract type ODESolution <: GridapType end
+# @santiagobadia : Agreed
+# @santiagobadia : I think this type is not a TimeStepper for Gridap, it is
+# at a lower level, since it does not work with the FE machinery. The
+# TimeStepper should provide FEFunction
 
 # First time step
 function Base.iterate(u::ODESolution) # -> (t_n,u_n) or nothing
@@ -133,7 +158,7 @@ function Base.iterate(u::ODESolution,state) # -> (t_n,u_n) or nothing
 end
 
 struct GenericODESolution <: ODESolution
-  solver::ODESolver
+  solver::ODESolution
   op::ODEOperator
   u0::AbstractVector
   t0::Real
@@ -178,7 +203,7 @@ end
 
 struct BackwardEuler <: ODESolver
   nls::NonLinearSolver
-  dt::Float64
+  dt::Float64 # @santiagobadia : Previous comment about dt
 end
 
 function solve_step!(
@@ -218,6 +243,7 @@ function jacobian!(A::AbstractMatrix,op::BackwardEulerNonLinearOperator,x::Abstr
   jacobian_u!(A_u,op.odeop,op.tF,uF,vF)
   jacobian_v!(A_v,op.odeop,op.tF,uF,vF)
   A .= A_u + (1/op.dt)*A_v
+  # @santiagobadia : Does it work so simple? Implemented for CSX matrices?
 end
 
 function allocate_residual(op::BackwardEulerNonLinearOperator,x::AbstractVector)
@@ -234,4 +260,6 @@ function zero_initial_guess(::Type{T},op::BackwardEulerNonLinearOperator) where 
   x0
 end
 
-
+# I think we can create the machinery for ODE solvers easily, using more or less
+# what we have above. But there is still a layer that combines it with the FE
+# machinery in Gridap. 
