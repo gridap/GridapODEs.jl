34b91edd878c1614393f71721e54635a60b8da28
Santiago Badia <santiago.badia@monash.edu>
2020-03-31 17:02:46 +1100

    Update TransientFEOperators.jl

--- src/FETimeSteppers/TransientFEOperators.jl
+++ src/FETimeSteppers/TransientFEOperators.jl
@@ -5,7 +5,7 @@ function jacobian_unk_t!(A::AbstractMatrix,op::FEOperatorFromTerms,uh)
   @assert is_a_fe_function(uh)
   du = get_cell_basis(op.trial)
   v = get_cell_basis(op.test)
-  cellmats, cellidsrows, cellidscols = collect_cell_jacobian(uh,uh_t,du_t,v,op.terms)
+  cellmats, cellidsrows, cellidscols = collect_cell_jacobian_unk_t(uh,uh_t,du_t,v,op.terms)
   assemble_matrix!(A,op.assem, cellmats, cellidsrows, cellidscols)
   A
 end
