0869bc2d9d93c39dfdbe29f98cbdda61a641df58
Santiago Badia <santiago.badia@monash.edu>
2020-04-06 16:18:07 +1000

    Transient FE Operator

--- src/TransientFETools/TransientFEOperators.jl
+++ src/TransientFETools/TransientFEOperators.jl
@@ -58,64 +58,84 @@ function TransientFEOperator(trial::Union{FESpace,TransientTrialFESpace},
   TransientFEOperatorFromTerms(trial,tr0,test,assem,terms)
 end
 
-function (tfes::TransientFEOperatorFromTerms)(t::Real)
-  FEOperator
-  # trial(t),test,assem,terms
-  # return a fe_operator
+function TransientFEOperator(trial::Union{FESpace,TransientTrialFESpace},
+  test::FESpace,terms)
+  # @santiagobadia : The time step value does not provide much info here...
+  # so I can just use the one at 0.0, but not sure... we want to keep the
+  # same assembler all the time...
+  assem = SparseMatrixAssembler(test,trial_0)
+  TransientFEOperatorFromTerms(trial,test,assem,terms)
 end
+
+# function (tfes::TransientFEOperatorFromTerms)(t::Real)
+#   FEOperator
+#   # trial(t),test,assem,terms
+#   # return a fe_operator
+# end
 # see comment above, I don't think it has much sense
 
 get_test(op::TransientFEOperatorFromTerms,t) = op.test
 
 get_trial(op::TransientFEOperatorFromTerms,t) = op.trial(t)
 
-function residual!(b::AbstractVector,op::TransientFEOperatorFromTerms,t,uh,uht)
-  # @santiagobadia : The uh and uht here are FEFunctions, created in the op
-  # below. However, it is unclear to me whether we can keep the residual!
-  # signature of FEOperator, since we need t to evaluate a transient operator
-  # and both uh and uht... I have changed the method interface !!! But now it
-  # should NOT subtype FEOperator !!!
-  @notimplemented #yet
-  A
-end
-
-function jacobian!(A::AbstractMatrix,op::TransientFEOperatorFromTerms,t,uh,uht)
-  @notimplemented #yet
-  A
-end
-
-function jacobian_t!(A::AbstractMatrix,op::TransientFEOperatorFromTerms,t,uh,uht)
+function allocate_residual(op::TransientFEOperatorFromTerms,uh)
   @assert is_a_fe_function(uh)
-  du = get_cell_basis(op.trial)
-  # This is not going to work, even though not needed Dir data here
-  # @santiagobadia: get_trial can be expensive, how can we reuse its call?
   v = get_cell_basis(op.test)
-  # to be implemented... collect_cell_jacobian_t
-  cellmats, cellidsrows, cellidscols = collect_cell_jacobian_t(uh,uh_t,du_t,v,op.terms)
+  _, cellids = collect_cell_residual(uh,v,op.terms)
+  # @santiagobadia : How do we do this? This assembler should be in the
+  #
+  allocate_vector(op.assem, cellids)
+end
+
+function residual!(b::AbstractVector,op::TransientFEOperatorFromTerms,
+  t::Real,uh,uh_t)
+  @assert is_a_fe_function(uh)
+  @assert is_a_fe_function(uh_t)
+  v = get_cell_basis(op.test)
+  cellvecs, cellids = collect_cell_residual(t,uh,uh_t,v,op.terms)
+  # @santiagobadia : not sure op will have Assembler
+  assemble_vector!(b,op.assem, cellvecs, cellids)
+  b
+end
+
+function allocate_jacobian(op::TransientFEOperatorFromTerms,uh)
+  @assert is_a_fe_function(uh)
+  # @santiagobadia : not sure op will have Assembler
+  du = get_cell_basis(op.trial_0)
+  # this is not a test function, it needs time... and it is not efficient
+  v = get_cell_basis(op.test)
+  _, cellidsrows, cellidscols = collect_cell_jacobian(uh,du,v,op.terms)
+  allocate_matrix(op.assem, cellidsrows, cellidscols)
+end
+
+function jacobian!(A::AbstractMatrix,op::FEOperatorFromTerms,
+  t::Real,uh,uh_t)
+  @assert is_a_fe_function(uh)
+  @assert is_a_fe_function(uh_t)
+  du = get_cell_basis(op.trial_0)
+  v = get_cell_basis(op.test)
+  cellmats, cellidsrows, cellidscols = collect_cell_jacobian(t,uh,du,v,op.terms)
   assemble_matrix!(A,op.assem, cellmats, cellidsrows, cellidscols)
   A
 end
 
-function get_ode_operator(feop::TransientFEOperator)
-  ODEOpFromFEOp(feop)
+function jacobian_t!(A::AbstractMatrix,op::TransientFEOperatorFromTerms,
+  t::Real,uh,uh_t)
+  @assert is_a_fe_function(uh)
+  @assert is_a_fe_function(uh_t)
+  du = get_cell_basis(op.trial_0)
+  # This is not going to work, even though not needed Dir data here
+  # @santiagobadia: get_trial can be expensive, how can we reuse its call?
+  v = get_cell_basis(op.test)
+  # to be implemented... collect_cell_jacobian_t
+  cellmats, cellidsrows, cellidscols = collect_cell_jacobian_t(t,uh,uh_t,du_t,v,op.terms)
+  assemble_matrix!(A,op.assem, cellmats, cellidsrows, cellidscols)
+  A
 end
 
-struct ODEOpFromFEOp <: ODEOperator
-  feop::TransientFEOperator
-end
-
-function residual!(b::AbstractVector,op::ODEOpFromFEOp,t::Real,uhF::AbstractVector,uhtF::AbstractVector)
-  # @santiagobadia : uh are just free dof array, idem uht
-  # Here we should think about strong bcs
-  residual!(b,op.feop,t,uh,uht)
-end
-
-# EvaluationFunction not FEFunction
-
-function jacobian!(A::AbstractMatrix,op::ODEOpFromFEOp,t,uh,uht)
-  jacobian!(A,op.feop,t,uh,uht)
-end
-
-function jacobian_t!(A::AbstractMatrix,op::ODEOpFromFEOp,t,uh,uht)
-  jacobian_t!(A,op.feop,t,uh,uht)
-end
+# """
+# """
+# function FEOperator(trial::FESpace,test::FESpace,terms::FETerm...)
+#   assem = SparseMatrixAssembler(test,trial)
+#   FEOperator(trial,test,assem,terms...)
+# end
