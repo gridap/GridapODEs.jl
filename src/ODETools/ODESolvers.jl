48a1fec4bff7b8cf33e706bfccfd2c17c7e85fd9
Santiago Badia <santiago.badia@monash.edu>
2020-04-12 13:15:58 +1000

    cleaning up

--- src/ODETools/ODESolvers.jl
+++ src/ODETools/ODESolvers.jl
@@ -1,7 +1,8 @@
 # Now, we need an abstract type representing a numerical discretization scheme
 # for the ODE
 """
-Represents a map that given (t_n,u_n) returns (t_n+1,u_n+1) and cache
+Represents a map that given (t_n,u_n) returns (t_n+1,u_n+1) and cache for the
+corresponding `ODEOperator` and `NonlinearOperator`
 """
 abstract type ODESolver <: GridapType end
 
@@ -19,9 +20,5 @@ function test_ode_solver(solver::ODESolver,op::ODEOperator,u0,t0,tf)
   solution = solve(solver,op,u0,t0,tf)
   test_ode_solution(solution)
 end
-
-# I think we can create the machinery for ODE solvers easily, using more or less
-# what we have above. But there is still a layer that combines it with the FE
-# machinery in Gridap.
 
 include("BackwardEuler.jl")
