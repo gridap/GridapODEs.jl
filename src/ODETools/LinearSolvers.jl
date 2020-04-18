245d844bdbd7a05a0aa76770754c500c94ab0867
Santiago Badia <santiago.badia@monash.edu>
2020-04-18 17:52:57 +1000

    Fixed bug

--- src/ODETools/LinearSolvers.jl
+++ src/ODETools/LinearSolvers.jl
@@ -8,7 +8,7 @@ function solve!(x::AbstractVector,nls::NonlinearSolver,op::NonlinearOperator,cac
   solve!(x,nls,op)
 end
 
-function solve!(x::AbstractVector,nls::NonlinearSolver,op::NonlinearOperator,cache,isb,isA)
+function solve!(x::AbstractVector,nls::NonlinearSolver,op::NonlinearOperator,cache)
   solve!(x,nls,op,cache)
 end
 
@@ -26,6 +26,25 @@ function solve!(x::AbstractVector,
   LinearSolverCache(A,b,ns)
 end
 
+function solve!(x::AbstractVector,
+                ls::LinearSolver,
+                op::NonlinearOperator,
+                cache)
+  x .= zero(eltype(x))
+  b = cache.b
+  A = cache.A
+  ns = cache.ns
+  # if !isbct
+  # we must recalculate it
+  residual!(b, op, x)
+  # end
+  # if !isAct
+  numerical_setup!(ns,A)
+  # end
+  broadcast!(*,b,b,-1)
+  solve!(x,ns,b)
+  cache
+end
 
 # function solve!(x::AbstractVector,
 #                 ls::LinearSolver,
