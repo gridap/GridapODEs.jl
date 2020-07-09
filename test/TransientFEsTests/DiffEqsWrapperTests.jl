a73a73e47aaaef3bb2805bf53e7cf69f5e031ad8
Santiago Badia <santiago.badia@monash.edu>
2020-07-09 19:14:46 +1000

    sundials test

--- test/TransientFEsTests/DiffEqsWrapperTests.jl
+++ test/TransientFEsTests/DiffEqsWrapperTests.jl
@@ -1,4 +1,4 @@
-# module DiffEqsWrapperTests
+module DiffEqsWrapperTests
 
 using Gridap
 using ForwardDiff
@@ -13,27 +13,28 @@ using DifferentialEquations
 import Gridap: ∇
 import GridapODEs.TransientFETools: ∂t
 
-##
+# Heat equation
 
-θ = 1.0
-
-# @santiagobadia : u(x,t) = t not working with Sundials solver, not right sol?
-u(x,t) = 1.0#(1.0-x[1])*x[1]*(1.0-x[2])*x[2]*t
+# With this manufactured solution everything OK
+u(x,t) = 1.0
 
 # @santiagobadia : Even though the problem that we will solve is linear, the
-# Sundials solvers seems to have no convergence problems in the nonlinear solver
-
-u(x,t) = t#(1.0-x[1])*x[1]*(1.0-x[2])*x[2]*t
-
-
+# Sundials solvers seems to have convergence issues in the nonlinear solver (?)
+# It seems to work for partitions of at most (3,3), then it returns errors
+# [IDAS ERROR]  IDACalcIC
+# Newton/Linesearch algorithm failed to converge.
+# That is not surprising, because I guess that the J in jacobian is a
+# *dense* matrix, but not in Gridap (or any FEM solver). To clarify this
+# issue.
+u(x,t) = t
 
 u(t::Real) = x -> u(x,t)
-∂tu = ∂t(u)
+# ∂tu = ∂t(u)
 
 f(t) = x -> ∂t(u)(x,t)-Δ(u(t))(x)
 
 domain = (0,1,0,1)
-partition = (4,4)
+partition = (3,3)
 model = CartesianDiscreteModel(domain,partition)
 
 order = 1
@@ -47,7 +48,6 @@ trian = Triangulation(model)
 degree = 2*order
 quad = CellQuadrature(trian,degree)
 
-#
 a(u,v) = ∇(v)⋅∇(u)
 b(v,t) = v*f(t)
 
@@ -65,21 +65,14 @@ dt = 0.1
 U0 = U(0.0)
 uh0 = interpolate_everywhere(U0,u(0.0))
 
-ls = LUSolver()
-using Gridap.Algebra: NewtonRaphsonSolver
-nls = NLSolver(ls;show_trace=true,method=:newton)
-odes = ThetaMethod(ls,dt,θ)
-solver = TransientFESolver(odes)
-sol_t = Gridap.solve(solver,op,uh0,t0,tF)
-
 #DiffEq wrapper
 
 ode_op = get_algebraic_operator(op)
 ode_cache = allocate_cache(ode_op) # Not acceptable in terms of performance
 
 function residual(res,du,u,p,t)
-  # Problem with closure if ode_c is named ode_cache
-  # @santiagobadia : Clarify this point
+  # TO DO: Problem with closure if ode_c is named ode_cache
+  # TO DO: Improve update_cache! st do nothing if same time t as in the cache
   ode_c = GridapODEs.ODETools.update_cache!(ode_cache,ode_op,tθ)
   GridapODEs.ODETools.residual!(res,ode_op,t,u,du,ode_c)
 end
@@ -91,23 +84,25 @@ function jacobian(jac,du,u,p,gamma,t)
   GridapODEs.ODETools.jacobian_t!(jac,ode_op,t,u,du,gamma,ode_c)
   GridapODEs.ODETools.jacobian!(jac,ode_op,t,u,du,ode_c)
 end
-
 #end wrapper
 
-u0 = get_free_values(uh0)
-tθ = 1.0
-dtθ = dt*θ
 
+# Check
+θ = 1.0
+tθ = 1.0
+u0 = get_free_values(uh0)
+dtθ = dt*θ
 r = GridapODEs.ODETools.allocate_residual(ode_op,u0,ode_cache)
 J = GridapODEs.ODETools.allocate_jacobian(ode_op,u0,ode_cache)
-##
 residual(r,u0,u0,nothing,tθ)
 jacobian(J,u0,u0,nothing,(1/dtθ),tθ)
+#
 
 using Sundials
 tspan = (0.0,1.0)
 
-# To explore the Sundials solver options, e.g., BE with time step dt
+# To explore the Sundials solver options, e.g., BE with fixed time step dt
+# for comparison
 f_iip = DAEFunction{true}(residual;jac=jacobian)
 prob_iip = DAEProblem{true}(f_iip,u0,u0,tspan,differential_vars=[true])
 sol_iip = Sundials.solve(prob_iip, IDA(), reltol=1e-8, abstol=1e-8)
@@ -124,24 +119,14 @@ for i in take(integ,100)
       @show integ.u
 end
 
-# Issue 1: No control over the linear solver, we would like to be able to
-# provide certainly linear and possibly nonlinear solvers.
-# Let us assume that we just want to consider a fixed point algorithm
-# and we consider an implicit time integration of a nonlinear PDE.
-# Our solvers are efficient since they re-use cache among
-# nonlinear steps (e.g., symbolic factorization, arrays for numerical factorization)
-# and for the transient case in our library, we can also reuse all this
-# between time steps. Could we attain something like this using
-# DifferentialEquations?
-
-# Issue 2: We have to call twice to update_cache!, for residual and jacobian
-# at each time step
-
-# Issue 3: Iterator-like version as in GridapODEs.
-# HOWEVER, is this computation lazy? I don't think so, so we need to store
-# all time steps, e.g., for computing time-dependent functionals (drag or lift
-# in CFD, etc), which is going to incur in a lot of memory consumption.
-
+# GridapODEs version using Backward Euler
+θ = 1.0
+ls = LUSolver()
+using Gridap.Algebra: NewtonRaphsonSolver
+nls = NLSolver(ls;show_trace=true,method=:newton)
+odes = ThetaMethod(ls,dt,θ)
+solver = TransientFESolver(odes)
+sol_t = Gridap.solve(solver,op,uh0,t0,tF)
 
 l2(w) = w*w
 
@@ -157,4 +142,23 @@ for (uh_tn, tn) in sol_t
   @show get_free_values(uh_tn)
 end
 
-# end #module
+# Issue 1: How do I initialize my residual vector (not that important) and my
+# jacobian (sparse CSR matrix). Dense matrices cannot be used in FE codes.
+
+# Issue 2: No control over the (non)linear solver, we would like to be able to
+# provide certainly linear and possibly nonlinear solvers.
+# Let us assume that we just want to consider a fixed point algorithm
+# and we consider an implicit time integration of a nonlinear PDE.
+# Our solvers are efficient since they re-use cache among
+# nonlinear steps (e.g., symbolic factorization, arrays for numerical factorization)
+# and for the transient case in our library, we can also reuse all this
+# between time steps. Could we attain something like this using
+# DifferentialEquations/Sundials?
+
+
+# Issue 3: Iterator-like version as in GridapODEs.
+# HOWEVER, is this computation lazy? I don't think so, so we need to store
+# all time steps, e.g., for computing time-dependent functionals (drag or lift
+# in CFD, etc), which is going to incur in a lot of memory consumption.
+
+end #module
