e067ab9ab15935fff820873de01c17b334d4e334
Santiago Badia <santiago.badia@monash.edu>
2020-08-13 22:27:38 +1000

    added wrappers for sundials

--- test/DiffEqsWrappersTests/DiffEqsTests.jl
+++ test/DiffEqsWrappersTests/DiffEqsTests.jl
@@ -1,15 +1,16 @@
-module GridapFESolver
+module DiffEqsWrapperTests
 
 using Test
 using Gridap
+using GridapODEs
 using GridapODEs.ODETools
 using GridapODEs.TransientFETools
-using Gridap.FESpaces: get_algebraic_operator
-using GridapODEs
+using GridapODEs.DiffEqWrappers
 
-export fe_problem
-export solve_ode_gridap
-export diffeq_wrappers
+# using DifferentialEquations
+using Sundials
+using Gridap.Algebra: NewtonRaphsonSolver
+using Base.Iterators
 
 # FE problem (heat eq) using Gridap
 function fe_problem(u, n)
@@ -54,70 +55,7 @@ function fe_problem(u, n)
 
 end
 
-# Wrappers for residual and jacobian for DiffEqs
-function diffeq_wrappers(op)
-
-  ode_op = get_algebraic_operator(op)
-  ode_cache = allocate_cache(ode_op)
-
-  function residual(res, du, u, p, t)
-    # TO DO (minor): Improve update_cache! st do nothing if same time t as in the cache
-    # now it would be done twice (residual and jacobian)
-    ode_cache = GridapODEs.ODETools.update_cache!(ode_cache, ode_op, t)
-    GridapODEs.ODETools.residual!(res, ode_op, t, u, du, ode_cache)
-  end
-
-  function jacobian(jac, du, u, p, gamma, t)
-    ode_cache = GridapODEs.ODETools.update_cache!(ode_cache, ode_op, t)
-    z = zero(eltype(jac))
-    Gridap.Algebra.fill_entries!(jac, z)
-    GridapODEs.ODETools.jacobian_and_jacobian_t!(jac, ode_op, t, u, du, gamma, ode_cache)
-  end
-
-  function mass(mass, du, u, p, t)
-    ode_cache = GridapODEs.ODETools.update_cache!(ode_cache, ode_op, t)
-    z = zero(eltype(mass))
-    Gridap.Algebra.fill_entries!(mass, z)
-    GridapODEs.ODETools.jacobian_t!(mass, ode_op, t, u, du, 1.0, ode_cache)
-  end
-
-  function stiffness(stif, du, u, p, t)
-    ode_cache = GridapODEs.ODETools.update_cache!(ode_cache, ode_op, t)
-    z = zero(eltype(stif))
-    Gridap.Algebra.fill_entries!(stif, z)
-    GridapODEs.ODETools.jacobian!(stif, ode_op, t, u, du, ode_cache)
-  end
-
-  return residual, jacobian, mass, stiffness
-
-end
-
-# Allocate Jacobian
-function allocate_jac(op,u0)
-  ode_op = get_algebraic_operator(op)
-  ode_cache = allocate_cache(ode_op) # Not acceptable in terms of performance
-  return GridapODEs.ODETools.allocate_jacobian(ode_op, u0, ode_cache)
-end
-
-end #module
-
-
-module DiffEqsWrapperTests
-
-using Gridap
-using ForwardDiff
-using LinearAlgebra
-using Test
-using GridapODEs.ODETools
-using GridapODEs.TransientFETools
-using Gridap.FESpaces: get_algebraic_operator
-using GridapODEs
-using DifferentialEquations
-using Gridap.Algebra: NewtonRaphsonSolver
-using Base.Iterators
-
 # Solving the heat equation using GridapODEs and DiffEqs
-using Sundials
 tspan = (0.0, 1.0)
 
 u(x, t) = t
@@ -128,34 +66,33 @@ u(t) = x -> u(x, t)
 # solver (?). Ut returns errors
 # [IDAS ERROR]  IDACalcIC Newton/Linesearch algorithm failed to converge.
 # ISSUE 2: When I pass `jac_prototype` the code gets stuck
-S = Main.GridapFESolver
 
 n = 3 # cells per dim (2D)
-op, u0 = S.fe_problem(u,n)
+op, u0 = fe_problem(u,n)
 
 # Some checks
-residual, jacobian, mass, stiffness = S.diffeq_wrappers(op)
-J = S.allocate_jac(op,u0)
+res!, jac!, mass!, stif! = diffeq_wrappers(op)
+J = prototype_jacobian(op,u0)
 r = copy(u0)
 θ = 1.0; t0 = 0.0; tF = 1.0; dt = 0.1; tθ = 1.0; dtθ = dt*θ
-residual(r, u0, u0, nothing, tθ)
-jacobian(J, u0, u0, nothing, (1 / dtθ), tθ)
+res!(r, u0, u0, nothing, tθ)
+jac!(J, u0, u0, nothing, (1 / dtθ), tθ)
 
-K = S.allocate_jac(op,u0)
-M = S.allocate_jac(op,u0)
-stiffness(K, u0, u0, nothing, tθ)
-mass(M, u0, u0, nothing, tθ)
+K = prototype_jacobian(op,u0)
+M = prototype_jacobian(op,u0)
+stif!(K, u0, u0, nothing, tθ)
+mass!(M, u0, u0, nothing, tθ)
 # Here you have the mass matrix M
 
 @test (1/dtθ)*M+K ≈ J
 
 # To explore the Sundials solver options, e.g., BE with fixed time step dtd
-f_iip = DAEFunction{true}(residual; jac = jacobian)#, jac_prototype=J)
+f_iip = DAEFunction{true}(res!; jac = jac!)#, jac_prototype=J)
 # jac_prototype is the way to pass my pre-allocated jacobian matrix
 prob_iip = DAEProblem{true}(f_iip, u0, u0, tspan, differential_vars = [true])
 # When I pass `jac_prototype` the code get stuck here:
 sol_iip = Sundials.solve(prob_iip, IDA(), reltol = 1e-8, abstol = 1e-8)
-@show sol_iip.u
+# @show sol_iip.u
 
 # or iterator version
 integ = init(prob_iip, IDA(), reltol = 1e-8, abstol = 1e-8)
@@ -163,7 +100,7 @@ integ = init(prob_iip, IDA(), reltol = 1e-8, abstol = 1e-8)
 
 # Show using integrators as iterators
 for i in take(integ, 100)
-  @show integ.u
+  # @show integ.u
 end
 
 end # module
