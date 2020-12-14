f54b613e92cd9590a72ba5fb6579dea010be67c1
Santiago Badia <santiago.badia@monash.edu>
2020-12-15 09:46:44 +1100

    Avoiding Sundial errors

--- test/DiffEqsWrappersTests/DiffEqsTests.jl
+++ test/DiffEqsWrappersTests/DiffEqsTests.jl
@@ -90,7 +90,7 @@ f_iip = DAEFunction{true}(res!; jac = jac!)#, jac_prototype=J)
 # jac_prototype is the way to pass my pre-allocated jacobian matrix
 prob_iip = DAEProblem{true}(f_iip, u0, u0, tspan, differential_vars = [true,true,true,true])
 # When I pass `jac_prototype` the code get stuck here:
-sol_iip = Sundials.solve(prob_iip, IDA(), reltol = 1e-8, abstol = 1e-8)
+# sol_iip = Sundials.solve(prob_iip, IDA(), reltol = 1e-8, abstol = 1e-8)
 # @show sol_iip.u
 
 # or iterator version
