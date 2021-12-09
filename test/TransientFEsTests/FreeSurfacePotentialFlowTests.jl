03b65fb8ea9f06933ec408fa73d8e019ae18e446
Oriol Colomes <oriol.colomes@gmail.com>
2021-12-09 21:48:34 +0100

    Adding Free-surface with potential flow test (mixed-dimensional PDE)

--- test/TransientFEsTests/FreeSurfacePotentialFlowTests.jl
+++ test/TransientFEsTests/FreeSurfacePotentialFlowTests.jl
@@ -37,19 +37,17 @@ model = CartesianDiscreteModel(domain,partition;isperiodic=(true,false))
 labels = get_face_labeling(model)
 add_tag_from_tags!(labels,"bottom",[1,2,5])
 add_tag_from_tags!(labels,"free_surface",[3,4,6])
-bgface_to_mask = get_face_mask(labels,[3,4,6],1)
-model_Γ =BoundaryDiscreteModel(Polytope{1},model,bgface_to_mask)
 
 # Triangulation
-Ω = Triangulation(model)
-Γ = Triangulation(model_Γ)
+Ω = Interior(model)
+Γ = Boundary(model,tags="free_surface")
 dΩ = Measure(Ω,2*order)
 dΓ = Measure(Γ,2*order)
 
 # FE spaces
 reffe = ReferenceFE(lagrangian,Float64,order)
-V = TestFESpace(model,reffe,conformity=:H1)
-V_Γ = TestFESpace(model_Γ,reffe,conformity=:H1)
+V = TestFESpace(Ω,reffe,conformity=:H1)
+V_Γ = TestFESpace(Γ,reffe,conformity=:H1)
 U = TransientTrialFESpace(V)
 U_Γ = TransientTrialFESpace(V_Γ)
 X = TransientMultiFieldFESpace([U,U_Γ])
@@ -62,40 +60,47 @@ Y = MultiFieldFESpace([V,V_Γ])
 m((ϕt,ηt),(w,v)) = ∫( 0.5*(α/g*(w*ϕt) + v*ϕt) - (w*ηt) )dΓ
 a((ϕ,η),(w,v)) = ∫( ∇(ϕ)⋅∇(w) )dΩ + ∫( 0.5*(α*(w*η) + g*v*η) )dΓ
 b((w,v)) = ∫( 0.0*w )dΓ
-# op = TransientConstantFEOperator(m,a,b,X,Y)
+op_const = TransientConstantFEOperator(m,a,b,X,Y)
 
 # TransientFEOperator exploiting automatic differentiation (testing purposes)
 res(t,x,y) = m(∂t(x),y) + a(x,y) - b(y)
 jac(t,x,dx,y) = a(dx,y)
 jac_t(t,x,dxt,y) = m(dxt,y)
-op = TransientFEOperator(res,jac,jac_t,X,Y)
+op_trans = TransientFEOperator(res,jac,jac_t,X,Y)
+op_ad = TransientFEOperator(res,X,Y)
 
 # Solver
 ls = LUSolver()
-odes = ThetaMethod(ls,Δt,θ)
-solver = TransientFESolver(odes)
+ode_solver = ThetaMethod(ls,Δt,θ)
 
 # Initial solution
 x₀ = interpolate_everywhere([ϕₑ(0.0),ηₑ(0.0)],X(0.0))
 
-# Solution
-sol_t = solve(solver,op,x₀,t₀,tf)
+function test_operator(op)
+  # Solution
+  sol_t = solve(ode_solver,op,x₀,t₀,tf)
 
-# Post-process
-l2_Ω(w) = √(∑(∫(w*w)dΩ))
-l2_Γ(v) = √(∑(∫(v*v)dΓ))
-E_kin(w) = 0.5*∑( ∫(∇(w)⋅∇(w))dΩ )
-E_pot(v) = g*0.5*∑( ∫(v*v)dΓ )
-Eₑ = 0.5*g*ξ^2*L
+  # Post-process
+  l2_Ω(w) = √(∑(∫(w*w)dΩ))
+  l2_Γ(v) = √(∑(∫(v*v)dΓ))
+  E_kin(w) = 0.5*∑( ∫(∇(w)⋅∇(w))dΩ )
+  E_pot(v) = g*0.5*∑( ∫(v*v)dΓ )
+  Eₑ = 0.5*g*ξ^2*L
 
-tol = 1.0e-2
-for ((ϕn,ηn),tn) in sol_t
-  E = E_kin(ϕn) + E_pot(ηn)
-  error_ϕ = l2_Ω(ϕn-ϕₑ(tn))
-  error_η = l2_Γ(ηn-ηₑ(tn))
-  @test abs(E/Eₑ-1.0) <= tol
-  @test error_ϕ <= tol
-  @test error_η <= tol
+  tol = 1.0e-2
+  for ((ϕn,ηn),tn) in sol_t
+    E = E_kin(ϕn) + E_pot(ηn)
+    error_ϕ = l2_Ω(ϕn-ϕₑ(tn))
+    error_η = l2_Γ(ηn-ηₑ(tn))
+    @test abs(E/Eₑ-1.0) <= tol
+    @test error_ϕ <= tol
+    @test error_η <= tol
+  end
+  println("xxx")
 end
+
+test_operator(op_const)
+test_operator(op_trans)
+# test_operator(op_ad) # Not working yet
 
 end
