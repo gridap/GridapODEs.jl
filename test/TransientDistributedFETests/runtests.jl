2cc880690ae0224ea9627b5d66022be3d8170eb6
Oriol Colomes <oriol.colomes@gmail.com>
2022-01-11 21:30:16 +0100

    Adding tests

--- test/TransientDistributedFETests/runtests.jl
+++ test/TransientDistributedFETests/runtests.jl
@@ -10,4 +10,20 @@ using Test
 
 @testset "DistributedConstantFEOperatorsTests" begin include("DistributedConstantFEOperatorsTests.jl") end
 
+@testset "DistributedDGHeatEquationTests" begin include("DistributedDGHeatEquationTests.jl") end
+
+@testset "DistributedHeatEquationTests" begin include("DistributedHeatEquationTests.jl") end
+
+# missing interpolate_everywhere:
+#@testset "DistributedForwardEulerHeatEquationTests" begin include("DistributedForwardEulerHeatEquationTests.jl") end
+
+# missing FESpace(triangulation,...)
+#@testset "DistributedFreeSurfacePotentialFlowTests" begin include("DistributedFreeSurfacePotentialFlowTests.jl") end
+
+# missing _jacobian(...) for DistributedDomainContribution
+#@testset "DistributedHeatEquationAutoDiffTests" begin include("DistributedHeatEquationAutoDiffTests.jl") end
+
+# missing broadcasted implementation for `A+M ≈ K`
+#@testset "DistributedHeatVectorEquationTests" begin include("DistributedHeatVectorEquationTests.jl") end
+
 end
